006105fd350efa77f47b33c0b09abfe66b6a74b2
Justinkoech <your_email@example.com>
2025-03-19 12:23:13 +0300

    final commit

--- main.py
+++ main.py
@@ -4,55 +4,51 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+# Setup screen
 screen = Screen()
-screen.setup(600,600)
+screen.setup(600, 600)
 screen.bgcolor("black")
-screen.title("The snake game")
+screen.title("The Snake Game")
 screen.tracer(0)
 
+# Create game objects
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# Listen for key presses
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-
-
+# Main game loop
 game_is_on = True
-while game_is_on :
+while game_is_on:
     screen.update()
     time.sleep(0.1)
-    snake.move_forwards()
+    snake.move_forward()
 
-    #detect collision with food
-    if snake.head.distance(food) < 15 :
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #detect collision with wall
-    if snake.head.xcor()  > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
-        game_is_on = False
-        scoreboard.game_over()
+    # Detect collision with wall
+    if (
+        snake.head.xcor() > 280 or snake.head.xcor() < -280
+        or snake.head.ycor() > 280 or snake.head.ycor() < -280
+    ):
+        scoreboard.reset()
+        snake.reset()
 
-    #detect collision with tail
+    # Detect collision with tail
     for segment in snake.segments[1:]:
-            if snake.head.distance(segment) < 10:
-                game_is_on = False
-                scoreboard.game_over()
-        #if the head collides with any segment in the tail:
-             #trigger game_over
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
-
-
-
-
-
-
-
-
+# Exit game on click
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,44 +1,63 @@
 from turtle import Turtle
 
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]  # Define starting positions
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP, DOWN, LEFT, RIGHT = 90, 270, 180, 0
+
 
 class Snake:
     def __init__(self):
         self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
-        self.head = self.segments[0]  # Move this line after segments are created
 
     def add_segment(self, position):
         new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
-        new_segment.goto(position)  # Use the position passed to the method
+        new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000, 1000)  # Move off screen
+
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def extend(self):
-        # Add a new segment to the snake at the position of the last segment
+        """Add a new segment to the snake at the position of the last segment."""
         self.add_segment(self.segments[-1].position())
 
-    def move_forwards(self):
+    def move_forward(self):
+        """Move the snake forward by shifting each segment to the position of the one ahead."""
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.head.heading() != 270:
-            self.head.setheading(90)
+        """Change direction to UP if not currently moving DOWN."""
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        if self.head.heading() != 90:
-            self.head.setheading(270)
+        """Change direction to DOWN if not currently moving UP."""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        if self.head.heading() != 0:
-            self.head.setheading(180)
+        """Change direction to LEFT if not currently moving RIGHT."""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        if self.head.heading() != 180:
-            self.head.setheading(0)
+        """Change direction to RIGHT if not currently moving LEFT."""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
